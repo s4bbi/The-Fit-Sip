588e131430f4822e4e11e2f1dcfa27948e66bd03
Yashpreet Singh <singhyashpreet22@gmail.com>
2025-09-20 00:35:51 +0530

    link update

--- src/components/HomePage/VideoReviews.jsx
+++ src/components/HomePage/VideoReviews.jsx
@@ -81,7 +81,7 @@ export default function VideoReviews() {
         {/* Community Button */}
         <div className="text-center mt-10">
           <a
-            href="https://drive.google.com/your-menu-link" // 🔗 replace with your actual Google Drive PDF link
+            href="https://chat.whatsapp.com/JiJ5ITO6XlhGhZ6NUutXRR" // 🔗 replace with your actual Google Drive PDF link
             target="_blank"
             rel="noopener noreferrer"
             className="inline-flex items-center gap-2 bg-green text-white px-6 md:px-12 py-3 rounded-full font-sB 
--- src/data/saladsContent.js
+++ src/data/saladsContent.js
@@ -1,8 +1,9 @@
 // src/data/shotsContent.js
 
 // Import shot images (replace with actual filenames in your assets folder)
-import saladbowl1 from "../assets/Images/saladbowl1.jpg";
-import saladbowl2 from "../assets/Images/saladbowl2.jpg";
+import saladbowl1 from "../assets/Images/saladbowl1.png";
+import saladbowl2 from "../assets/Images/saladbowl2.png";
+import saladbowl3 from "../assets/Images/saladbowl3.png";
 
 // Replace with your WhatsApp number
 const whatsappBase = "https://wa.me/9036024955?text=";
@@ -13,18 +14,27 @@ const shotsContent = [
     title: "GREEN FALAFEL CRUNCH",
     ingredients: "Falafel, cucumber, bell peppers...",
     calories: "Calories (Approx.) 480",
-    price: "₹149",
+    price: "₹159",
     image: saladbowl1,
     link: `${whatsappBase}Hi, I would like to order GREEN FALAFEL CRUNCH shot.`,
   },
   {
-    id: 1,
+    id: 2,
     title: "THE SPROUT HARVEST",
     ingredients: "Sprouts, tomatoes, capsicum...",
     calories: "Calories (Approx.) 170",
-    price: "₹129",
+    price: "₹149",
     image: saladbowl2,
     link: `${whatsappBase}Hi, I would like to order THE SPROUT HARVEST shot.`,
+  },
+  {
+    id: 3,
+    title: "PROTIEN MEX BOWL",
+    ingredients: "Hearty mix of rajma, chickpeas, corn...",
+    calories: "Calories (Approx.) 170",
+    price: "₹169",
+    image: saladbowl3,
+    link: `${whatsappBase}Hi, I would like to order PROTIEN MEX BOWL shot.`,
   },
 ];
 
